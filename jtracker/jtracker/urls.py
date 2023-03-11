cf59fb24a8399fbe688a44a13394ab4669af25e7
Andrew Shiels <ashiels@mtdata.com.au>
2023-03-11 22:14:20 +1100

    set default page to Welcome

--- jtracker/jtracker/urls.py
+++ jtracker/jtracker/urls.py
@@ -20,5 +20,6 @@ from site.views import welcome
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('welcome.html', welcome)
+    url('welcome.html', welcome),
+    url('', welcome)
 ]
